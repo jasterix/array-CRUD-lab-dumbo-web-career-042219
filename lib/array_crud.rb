8de1f3fc7040d95851635ceb7a76c9bb856751ee
Jasterix C <loune.calixte@gmail.com>
2019-04-01 07:18:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,13 +23,14 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  i = index_number
+  my_array = array[i]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[]-1
 end
